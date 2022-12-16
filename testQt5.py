b2bba9cf02a605ed34438640ac25da84e7316e4d
Solenne Rouillard <solenne.rouillard72@gmail.com>
2022-12-16 11:34:07 +0100

    >> Ajout et modification du Completer << Désormais l'auto-complétion fonctionne avec les termes de notre liste HPO. Un paramétrage a été réalisé pour faire en sorte que : - les lignes suggérés correspondent au texte saisie, peut importe sa localisation - que les accents soit acceptés (avec utf-8) - qu'il n'y est pas de sensibilité au niveau majuscule/minuscule (avec "sco" on peut trouver "Scoliose")

--- testQt5.py
+++ testQt5.py
@@ -1,26 +1,37 @@
 import sys
 from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QDialog, QListWidgetItem, QCompleter, QLineEdit, QWidget
 from PyQt5.uic import loadUi
+from numpy import loadtxt
+import codecs
 
 class fenetre(QWidget):
     def __init__(self):
+        #Création et paramétrage de la fenêtre :
         super(fenetre, self).__init__()
         loadUi('fenetre.ui',self)
         self.setWindowTitle('Notre Interface')
 
-        #Pour l'auto-complétion :
-        list_autocompletion = ["coucou", "banana", "anana"]
-        completer = QCompleter(list_autocompletion)
+        #Création de la liste des mots qui seront suggérés à l'utilisateur :
+        fichier_HPO = codecs.open("HPO_FR.txt", encoding='utf-8') #pour définir le fichier + accepter les accents avec 'utf-8'
+        list_autocompletion = loadtxt(fichier_HPO, dtype=str, comments="#", delimiter="\n", unpack=False)
 
-        self.lineedit = QLineEdit()
-        self.lineedit.setCompleter(completer)
+        #Création du 'completer' associé à la liste des mots/suggestions:
+        completer = QCompleter(list_autocompletion)
+        #Initialisation du mode de filtrage pour les suggestions
+        #Ici 'MatchContains' permets de suggérer toutes les lignes contenant la saisie, peut importe sa position dans la ligne
+        #Par exemple, si on saisie "sco" en FR, cela va nous retourner "Scotome", "Scoliose", "Faible score APGAR", ...
+        completer.setFilterMode(Qt.MatchContains)
+        #Et faire en sorte qu'il n'y ait pas de soucis avec les majuscules/minuscules
+        #Par exemple, si on saisie "sco" en FR, cela peut quand même retourner le terme "Scoliose" (qui commence par une majuscule)
+        completer.setCaseSensitivity(Qt.CaseInsensitive)
+
+        #Ajout du completer à notre input pour la saisie "lineEdit"
+        self.lineEdit.setCompleter(completer)
 
         for txt in list_pheno:
             QListWidgetItem(txt, self.listWidget)
-
-#Liste dynamique :
-#https://stackoverflow.com/questions/35074199/filling-a-qlistwidget-with-elements-from-a-dynamic-list
 
 list_pheno = ["coucou","banana","anana"]
 
@@ -28,3 +39,9 @@ app=QApplication(sys.argv)
 widget=fenetre()
 widget.show()
 sys.exit(app.exec_())
+
+#Liste dynamique :
+#https://stackoverflow.com/questions/35074199/filling-a-qlistwidget-with-elements-from-a-dynamic-list
+
+#Manuel pour PyQt5 :
+#https://doc.qt.io/qtforpython-5/PySide2/QtWidgets/QCompleter.html#PySide2.QtWidgets.PySide2.QtWidgets.QCompleter.setFilterMode
