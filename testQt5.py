5a5d52dcdfd78bcc0fb1f447ac2c7733ce19341a
Solenne Rouillard <solenne.rouillard72@gmail.com>
2023-01-20 10:59:35 +0100

    Suppression d'ancien code inutile

--- testQt5.py
+++ testQt5.py
@@ -16,25 +16,6 @@ class fenetre(QMainWindow):
         self.setMinimumSize(800, 500) #pour la taille minimum de la fenetre / @param : self.setMinimumSize(width, height)
         #self.setFixedSize(self.size()); #pour ne pas pouvoir redimensionner la fenetre
 
-        #================= POUR L'AUTO-COMPLETION =====================================================================#
-        #Création de la liste des mots qui seront suggérés à l'utilisateur :
-        #fichier_HPO = codecs.open("HPO_FR.txt", encoding='utf-8') #pour définir le fichier + accepter les accents avec 'utf-8'
-        #list_autocompletion, trash = loadtxt(fichier_HPO, dtype=str, comments="$", delimiter="#", unpack=True)
-        #print(list_autocompletion)
-
-        #Création du 'completer' associé à la liste des mots/suggestions:
-        #completer = QCompleter(list_autocompletion)
-        #Initialisation du mode de filtrage pour les suggestions
-        #Ici 'MatchContains' permets de suggérer toutes les lignes contenant la saisie, peut importe sa position dans la ligne
-        #Par exemple, si on saisie "sco" en FR, cela va nous retourner "Scotome", "Scoliose", "Faible score APGAR", ...
-        #completer.setFilterMode(Qt.MatchContains)
-        #Et faire en sorte qu'il n'y ait pas de soucis avec les majuscules/minuscules
-        #Par exemple, si on saisie "sco" en FR, cela peut quand même retourner le terme "Scoliose" (qui commence par une majuscule)
-        #completer.setCaseSensitivity(Qt.CaseInsensitive)
-
-        #Ajout du completer à notre input pour la saisie "lineEdit"
-        #self.lineEdit.setCompleter(completer)
-
         #Setting de la langue de l'application :
         self.on_francaisButton_clicked()
 
