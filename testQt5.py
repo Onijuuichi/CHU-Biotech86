a7d3744774b3e135536c1e9f52d0c061d556ceae
Solenne Rouillard <solenne.rouillard72@gmail.com>
2023-03-01 17:19:10 +0100

    TEST

--- testQt5.py
+++ testQt5.py
@@ -8,7 +8,7 @@ from PyQt5.uic import loadUi
 from numpy import loadtxt
 import codecs
 import re
-#import pandas as pd #python -m pip install pandas
+from array import *
   
 class fenetre(QMainWindow):
     def __init__(self):
@@ -27,7 +27,6 @@ class fenetre(QMainWindow):
         self.completer.activated.connect(self.handleCompletion) #Set a custom completion handle
 
         self.label_warning.hide() #Hide error message
-
 
     @pyqtSlot()
     def on_addButton_clicked(self):
@@ -89,13 +88,14 @@ class fenetre(QMainWindow):
             
             #If this element is not already in the saved list (based on the HPO term - splitted_text[1])
             #Param : splitted_text[0] contains the phenotypic description, and splitted_text[1] the HPO term
+            #print("splitted 0 : "+splitted_text[0]+" |||| splitted 1 : "+splitted_text[1]) #Check if it is working
             if not any([splitted_text[1] in word for word in saved_items]):
                 #Then it can be saved in the auxilary list
                 # + Editing the text line in order to have quotation marks (") before and after the phenotype
                 edited_text="\""+splitted_text[0]+"\"\t"+splitted_text[1]
                 saved_items += [edited_text]
             
-        #print(saved_items) just to check
+        #print(saved_items) #Check if the list items are correct
                   
         #When all elements are retrieved, we can ask where to create the CVS file :
         file_name, _ = QFileDialog.getSaveFileName(self, 'Save File', os.getenv('HOME'),"Comma-separated file (*.csv)")
@@ -106,6 +106,68 @@ class fenetre(QMainWindow):
                 #For each element in the saved list:
                 for element in saved_items:
                     f.write(element+"\n") #Add it in the file
+
+        #SAVING THE ELEMENTS AND UPDATING THEIR COUNTER
+        for element in saved_items:
+            #Also add one to the index of each element
+            line = element.replace("\"","")
+            #Get the index of the line where there is this element, in the initial list of terms
+            index_element = list(self.list_autocompletion).index(line)
+            #print(list(self.list_autocompletion).index(line)) #to check
+            self.list_autocompletion
+
+            #Then update the specific file, based on the choosen language
+            #We didn't do both files, bc they are not the same number of lines
+            #Bc our method is based on the line index, we can't do both
+            if self.language == "FR":
+                self.update_file_content(index_element, "HPO_FR.txt")
+            else:
+                self.update_file_content(index_element, "HPO_EN.txt")
+
+        #Then we refresh the completer based on the language of the app
+        self.set_autocompleter(self.language)
+
+    #@pyqtSlot()
+    def update_file_content(self, index_element, file_name): 
+        #EDIT THE INDEX HERE
+        with open(file_name,'r', encoding="utf-8") as txt:
+            file_content=txt.readlines() #Save every line of the file in a variable
+            old_line = re.split('#', file_content[index_element]) #Get the line of the specific element to be updated
+            updated_counter=int(old_line[0])+1 #Add a one to the counter of this element
+            updated_line = str(updated_counter)+"#"+old_line[1]+"#\n"#Remake the line with the new counter
+            #print(updated_line) #Check if the updated line is correct
+
+            file_content[index_element]=updated_line #Update the line based on the element index
+            #print(file_content[index_element]) #Check if line correctly updated
+
+            #Sort the content, to make the searched ones at the top
+            file_sorted = sorted(file_content, reverse=True)
+            
+            #Then we rewrite the file with the updated content : 
+            with open(file_name,'w', encoding="utf-8") as txt:
+                txt.writelines("_"+line for line in file_sorted)
+
+        #However, we had problems with a specific line : Anomalie de la suture métopique
+        #It kept going on the same line of another phenotype, so to prevent this
+        #We created a check to see if there is "#_" in the file,
+        #If so it indicates that two phenotype are on the same line :
+        #_0#Phenotype 1 HPO:0001#_0#Phenotype 2 HPO:0002#
+        #(prevent problems or blockage of the program)
+
+        #Open the file, and save its content
+        with open(file_name,'r', encoding="utf-8") as txt:
+            file_content=txt.read() 
+        
+        #Replace the "#_" by a "#\n" (it will put the second phenotype on another line)
+        file_corrected = file_content.replace('#_', '#\n')
+
+        #Then change also the "_" at the start of every line:
+        file_corrected = file_content.replace('_', '')
+        
+        #Now we can save the corrected content in the initial file by rewriting it
+        with open(file_name, 'w', encoding="utf-8") as file:
+            file.write(file_corrected)
+
 
     @pyqtSlot()
     def on_englishButton_clicked(self):
@@ -128,6 +190,13 @@ class fenetre(QMainWindow):
         self.tabWidget.setTabText(2,'Language')
 
         self.label_warning.setText('')
+        self.lineEdit.setText('')
+
+        self.listWidget.clear() #Removes all the element in the list
+        #We remove all the element in the list, bc there are not the same
+        #amount of lines in the french and the english file
+        #Example, if a line in the french file is not present in the english file
+        #There will be an error, because the comparison method is by comparind indexes
 
         self.set_autocompleter(self.language)
         self.reset_list_language(self.language)
@@ -153,24 +222,28 @@ class fenetre(QMainWindow):
         self.tabWidget.setTabText(2,'Langue')
 
         self.label_warning.setText('')
+        self.lineEdit.setText('')
+
+        self.listWidget.clear() #Removes all the element in the list
+        #Same explanation as in the on_englishButton_clicked(self) method
 
         self.set_autocompleter(self.language)
         self.reset_list_language(self.language)
 
     @pyqtSlot()
     def set_autocompleter(self, string):
-        #Check wich language is chosen:
+        #Check which language is chosen:
         if string=="FR":
             nom_fichier="HPO_FR.txt" #French language chosen, use the file with French terms
-            #print('Fichier en FR utiliser pour le completer') #to check
+            #print('Fichier en FR utiliser pour le completer') #Check
         else:
             nom_fichier="HPO_EN.txt" #English language chosen, use the file with English terms
-            #print('Fichier en EN utiliser pour le completer') #to check
+            #print('Fichier en EN utiliser pour le completer') #Check
 
         #================= FOR THE AUTO-COMPLETION =====================================================================#
         #Create terms list that will be suggested to the user:
         fichier_HPO = codecs.open(nom_fichier, encoding='utf-8') #define file and accept accents with 'utf-8'
-        self.list_autocompletion, trash = loadtxt(fichier_HPO, dtype=str, comments="$", delimiter="#", unpack=True)
+        self.index, self.list_autocompletion, trash = loadtxt(fichier_HPO, dtype=str, comments="$", delimiter="#", unpack=True)
         
         #Create the 'completer' linked to the suggestions terms list:
         self.completer = QCompleter(self.list_autocompletion)
@@ -183,31 +256,30 @@ class fenetre(QMainWindow):
         #For example, if we enter "sco" in FR, this can still return the term "Scoliosis" (which starts with a capital letter)
         self.completer.setCaseSensitivity(Qt.CaseInsensitive)
 
+        #Attaching the completer to the inpu (lineEdit) and activating it
         self.completer.setWidget(self.lineEdit)
         self.lineEdit.textChanged.connect(self.handleTextChanged)
         self.completer.activated.connect(self.handleTextChanged)
-        #self._completing = False
-        #Add the completer to our input for the "lineEdit" entry
-        #self.lineEdit.setCompleter(self.completer)
+        self.completer.activated.connect(self.handleCompletion) #Set a custom completion handle
                             
     @pyqtSlot()
     def reset_list_language(self, string):
         #Verify the language chosen, to set up the good terms:
         if string=="FR":
-            #before clicking on the button, it was in English
-            #the user wants it in French from now on
+            #Before clicking on the button, it was in English
+            #The user wants it in French from now on
             fichier_langue_apres="HPO_FR.txt" 
-            #fichierprint('FR')
+            #print('FR') #Check
 
         else:
             #before clicking on the button, it was in French
             #the user wants it in English from now on
             fichier_langue_apres="HPO_EN.txt" 
-            #fichierprint('EN')
+            #print('EN') #Check
         
         #Create phen/HPO terms list:
         fichier_HPO = codecs.open(fichier_langue_apres, encoding='utf-8') #define file and accept accents with 'utf-8'
-        self.liste_termes, trash = loadtxt(fichier_HPO, dtype=str, comments="$", delimiter="#", unpack=True)
+        self.index, self.liste_termes, trash = loadtxt(fichier_HPO, dtype=str, comments="$", delimiter="#", unpack=True)
         
         #For each term in the list:
         for index in range(self.listWidget.count()):
@@ -218,7 +290,7 @@ class fenetre(QMainWindow):
             
             matching = [s for s in self.liste_termes if ligne[-1] in s]
             self.listWidget.item(index).setText(str(matching[0]))
-            #print(matching) #verification // depending on the term, sometimes there are several, so we put the index 1
+            #print(matching) #Check
 
     #Method based on the following websites:
     #https://stackoverflow.com/questions/16158715/globbing-input-with-qcompleter
@@ -255,10 +327,7 @@ class fenetre(QMainWindow):
                 #Car sinon la fonction "find_near_matches" beug
                 #par exemple: il y a un blocage si on "dz" par erreur
                 if len(text) > 2:
-                    fichier="HPO_FR.txt"
-                    
-                    #fichier_HPO = codecs.open(fichier, encoding='utf-8') #define file and accept accents with 'utf-8'
-                    #liste_termes, trash = loadtxt(fichier_HPO, dtype=str, comments="$", delimiter="#", unpack=True)
+                    #fichier="HPO_FR.txt"
                     
                     #We take the terms in the list :
                     liste_str='\n'.join(self.liste_termes)
@@ -266,7 +335,7 @@ class fenetre(QMainWindow):
                     text = text.replace(" ", "_" )
                     liste_str = liste_str.replace(" ", "_" )
 
-                    #TO SHOW EVERYTHING: print(liste_str)
+                    #print(liste_str) #Check if the list is correct
                     near_matches = find_near_matches(text, liste_str, max_l_dist=1)
                 
                     #Si le dernier caractère entré est un espace (signalé par "_"), 
@@ -294,7 +363,7 @@ class fenetre(QMainWindow):
             if found:
                 self.completer.complete()
             else:
-                self.completer.popup().hide()
+                self.completer.popup().hide() 
 
     #METHODE POUR GERER L'AUTOCOMPLETION
     #Sans cette méthode, il n'y aura pas de d'autocomplétion lorsqu'on clique sur un élément du completer
